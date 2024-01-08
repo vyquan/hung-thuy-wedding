c30edc11a8a06e8b52615b6a34707f2395b470ac
QuanVV <vyquan99a10@gmail.com>
2024-01-08 19:33:47 +0700

    feat: add seo head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Hưng ❤️ Thuỳ",
-  description: "Chúng mình thành đôi",
+  description: "Thiệp Mời Hôn Lễ Ngọc Hưng & Hoàng Thuỳ (12/01/2024)",
   icons: {
     icon: "/favicon.ico",
   },
@@ -37,11 +37,30 @@ const imperialScript = Imperial_Script({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="/images/hungthuy/ht-banner-min.jpg" />
-      <meta property="og:title" content="Hưng ❤️ Thuỳ" />
-      <meta property="og:description" content="Chúng mình thành đôi" />
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <title>Thiệp Mời Hôn Lễ Ngọc Hưng &amp; Hoàng Thuỳ (12/01/2024)</title>
+        <meta
+          name="description"
+          content="Trân trọng kính mời Quý khách tới tham dự Hôn Lễ của chúng tôi vào ngày 12/01/2024 (02/12 Âm Lịch). Sự hiện diện của Quý khách là niềm vinh hạnh của chúng tôi! Rất hân hạnh được đón tiếp."
+        />
+        <meta property="og:locale" content="vi_VN" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Thiệp Mời Hôn Lễ Ngọc Hưng &amp; Hoàng Thuỳ (12/01/2024)" />
+        <meta
+          property="og:description"
+          content="Trân trọng kính mời Quý khách tới tham dự Hôn Lễ của chúng tôi vào ngày 12/01/2024 (02/12 Âm Lịch). Sự hiện diện của Quý khách là niềm vinh hạnh của chúng tôi! Rất hân hạnh được đón tiếp."
+        />
+        <meta property="og:url" content="https://hung-thuy-wedding.vercel.app/" />
+        <meta property="og:site_name" content="Thiệp Cưới Online" />
+        <meta
+          property="og:image"
+          content="https://hung-thuy-wedding.vercel.app/images/hungthuy/ht-banner-crop_optimized.jpg"
+        />
+        <meta property="og:image:width" content="950" />
+        <meta property="og:image:height" content="633" />
+        <meta property="og:image:type" content="image/jpeg" />
+      </head>
       <body
         className={`${questrial.variable} ${parfumerieScriptPro.variable} ${dancingScript.variable} ${imperialScript.variable}`}
       >
